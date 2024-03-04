b485bb5288e04cd5ab855ff25325d842893ecaa0
Avaneesh Singh <Avaneesh.Singh@Geminisolutions.com>
2024-03-04 15:43:39 +0530

    -- board card --

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,8 @@ import { EmptySearch } from "./empty-search";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
+
 
 interface BoardListProps {
     orgId:string,
@@ -22,13 +24,22 @@ export const BoardList=({
 }:BoardListProps)=>{
     const data=useQuery(api.boards.get,{orgId});
 
-    if(data===undefined){
-        return(
-            <div>
-                Loading ...
+    if (data === undefined) {
+        return (
+          <div>
+            <h2 className="text-3xl">
+              {query.favorites ? "Favorite boards" : "Team boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+              <NewBoardButton orgId={orgId} disabled />
+              <BoardCard.Skeleton />
+              <BoardCard.Skeleton />
+              <BoardCard.Skeleton />
+              <BoardCard.Skeleton />
             </div>
+          </div>
         )
-    }
+      }
 
     if(!data?.length && query.search){
         return(
@@ -52,7 +63,8 @@ export const BoardList=({
             <h2 className="text-3xl">
                 {query.favorites?"Favorite Boards":"Team Boards"}
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 mt-8 pb-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 mt-8 pb-10 gap-5">
+                <NewBoardButton orgId={orgId}/>
                 {data?.map((board)=>(
                      <BoardCard key={board._id} id={board._id} title={board.title} imageUrl={board.imageUrl} authorId={board.authorId} authorName={board.authorName} createdAt={board._creationTime} orgId={board.orgId} isFavorite={false}/>
                 ))}
