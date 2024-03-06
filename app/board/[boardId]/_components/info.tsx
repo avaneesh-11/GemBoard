8a1567c3aa21b87b6b2e13d380428b8c2615fcad
Avaneesh Singh <Avaneesh.Singh@Geminisolutions.com>
2024-03-06 12:18:22 +0530

    -- canvas layout and Liveblocks setup --

---
+++ app/board/[boardId]/_components/info.tsx
@@ -0,0 +1,17 @@
+"use-client";
+
+export const Info=()=>{
+    return(
+        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+            Info
+        </div>
+    )
+}
+
+export const InfoSkeleton = () => {
+    return (
+      <div 
+        className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]"
+      />
+    );
+  };
